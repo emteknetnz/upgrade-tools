37cb13e0f6b1ebd8b0934499ce4b76a2db19f077
Steve Boyd <emteknetnz@gmail.com>
2019-09-13 11:41:58 +1200

    Show html table

--- code/tasks/AnalyseAssetsTask.php
+++ code/tasks/AnalyseAssetsTask.php
@@ -7,10 +7,69 @@ class AnalyseAssetsTask extends BuildTask
     /**
      * @param SS_HTTPRequest $request
      */
-    public function run($request)
+    public function run($request): void
     {
+        $sort = $request->getVar('sort') ?: 'MB';
+
+        // styles
+        $this->echoStyles();
+
         // assets
 
         // database
+        $this->showTables($sort);
+    }
+
+    private function showTables(string $sort): void
+    {
+        $lines = ['<th>' . implode('</th><th>', [
+            '<a href="?sort=Name">Name</a>',
+            '<a href="?sort=Rows">Rows</a>',
+            '<a href="?sort=MB">MB</a>'
+        ]) . '</th>'];
+        foreach ($this->getTableData($sort) as $r) {
+            $lines[] = '<td>' . implode('</td><td>', [$r['Name'], $r['Rows'], $r['MB']]) . '</td>';
+        }
+        echo '<table><tr>' . implode('</tr><tr>', $lines) . '</tr></table>';
+    }
+
+    private function getTableData(string $sort): array
+    {
+        $data = [];
+        foreach (DB::query("SHOW TABLE STATUS") as $r) {
+            $data[] = [
+                'Name' => $r['Name'],
+                'Rows' => $r['Rows'],
+                'MB' => round(($r[ "Data_length" ] + $r[ "Index_length" ]) / (1024 * 1024), 2)
+            ];
+        }
+        usort($data, function($a, $b) use ($sort) {
+            if (!isset($a[$sort])) {
+                return 0;
+            }
+            return $a[$sort] <=> $b[$sort];
+        });
+        if (in_array($sort, ['MB', 'Rows'])) {
+            $data = array_reverse($data);
+        }
+        return $data;
+    }
+
+    private function echoStyles(): void
+    {
+        echo <<<EOT
+            <style>
+                table {
+                    border-collapse: collapse;
+                }
+                th {
+                    text-align: left;
+                }
+                th, td {
+                    border: 1px solid #ccc;
+                    padding: 5px;
+                }
+            </style>
+EOT;
     }
 }
