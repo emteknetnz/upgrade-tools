f9f1199d26f8a88707aee0f01ccbec5383d1f206
Steve Boyd <emteknetnz@gmail.com>
2019-09-13 12:11:36 +1200

    Add directory analyser, make php 5.6 compat

--- code/tasks/AnalyseAssetsTask.php
+++ code/tasks/AnalyseAssetsTask.php
@@ -7,7 +7,7 @@ class AnalyseAssetsTask extends BuildTask
     /**
      * @param SS_HTTPRequest $request
      */
-    public function run($request): void
+    public function run($request)
     {
         // styles
         $this->echoStyles();
@@ -25,11 +25,67 @@ class AnalyseAssetsTask extends BuildTask
             $sort = $request->getVar('sort') ?: 'MB';
             $this->showDatabase($sort);
         }
-
-
     }
 
-    private function showDatabase(string $sort): void
+    private function showAssets()
+    {
+        var_dump($this->getDirContents(ASSETS_PATH));
+    }
+
+    /**
+     * @param string $dir
+     * @param array $results
+     * @return array
+     */
+    private function getDirContents($dir, &$results = array())
+    {
+        foreach(scandir($dir) as $key => $value){
+            if (in_array($value, ['.', '..'])) {
+                continue;
+            }
+            $path = realpath($dir . DIRECTORY_SEPARATOR . $value);
+
+            $parts = array_filter(explode(DIRECTORY_SEPARATOR, str_replace(ASSETS_PATH, '', dirname($path))));
+            $this->thinger($results, $parts);
+
+            if (is_dir($path)) {
+                if (!isset($results[$path])) {
+                    $results[$path] = [];
+                }
+                $this->getDirContents($path, $results);
+            } else {
+                $results[$path] = [
+                    'filename' => $value,
+                    'filesize' => filesize($path)
+                ];
+            }
+
+//            if (!is_dir($path)) {
+//                // filename
+//                // $results[] = $path;
+//                // filesize
+//                $results[] = filesize($path);
+//            } else {
+//                $this->getDirContents($path, $results);
+//                $results[] = $path;
+//            }
+        }
+        return $results;
+    }
+
+    /**
+     * @param array $results
+     * @param array $parts
+     */
+    private function thinger(array &$results, array $parts)
+    {
+        $c = count($parts);
+        if ($c > 1) {
+            //if (!)
+        }
+    }
+
+    private function showDatabase($sort)
     {
         $lines = ['<th>' . implode('</th><th>', [
             '<a href="?which=database&sort=Name">Name</a>',
@@ -42,7 +98,7 @@ class AnalyseAssetsTask extends BuildTask
         echo '<table><tr>' . implode('</tr><tr>', $lines) . '</tr></table>';
     }
 
-    private function getTableData(string $sort): array
+    private function getTableData($sort)
     {
         $data = [];
         foreach (DB::query("SHOW TABLE STATUS") as $r) {
@@ -56,15 +112,16 @@ class AnalyseAssetsTask extends BuildTask
             if (!isset($a[$sort])) {
                 return 0;
             }
-            return $a[$sort] <=> $b[$sort];
+            if (in_array($sort, ['MB', 'Rows'])) {
+                return $a[$sort] > $b[$sort] ? -1 : 1;
+            } else {
+                return strcasecmp($a[$sort], $b[$sort]);
+            }
         });
-        if (in_array($sort, ['MB', 'Rows'])) {
-            $data = array_reverse($data);
-        }
         return $data;
     }
 
-    private function echoOptions(): void
+    private function echoOptions()
     {
         echo <<<EOT
             <p>
@@ -75,7 +132,7 @@ EOT;
 
     }
 
-    private function echoStyles(): void
+    private function echoStyles()
     {
         echo <<<EOT
             <style>
